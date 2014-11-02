653d9f88b9113798182b0643dd868ede757b3e64
horechek <horechek@gmail.com>
2014-11-02 15:49:24 +0200

    added readme

--- ino/Serial.ino
+++ ino/Serial.ino
@@ -1,12 +1,28 @@
+/**
+ * Простой пример коммуникации между сишной прогой и ардуинкой. 
+ *
+ * Используется либа LibSerial, которую нужно сперва установить.
+ *
+ * ```sh
+ * sudo apt-get install libserial-dev
+ * ```
+ */
+
 int ledPin = 4;
 char incomingByte;
 
+/**
+ * 
+ */
 void setup()
 {
     Serial.begin(9600);
     pinMode(ledPin, OUTPUT);
 }
 
+/**
+ * 
+ */
 void loop()
 {
     if (Serial.available() > 0) {  //если есть доступные данные
@@ -17,7 +33,7 @@ void loop()
 
         if (incomingByte == '1') {
             digitalWrite(ledPin, HIGH);
-        } else if (incomingByte == '1') {
+        } else if (incomingByte == '0') {
             digitalWrite(ledPin, LOW);
         }
     }
